2e11ef608375f625b76a0d8cca3b6291e78572cc
Florent Kowalczuk <florent@MacBook-Pro-de-Florent.local>
2025-01-16 15:06:59 +0900

    commit message here

--- app/db/firebaseConfig.js
+++ app/db/firebaseConfig.js
@@ -1,3 +1,4 @@
+xxx
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'; // Ensure Firestore is imported
