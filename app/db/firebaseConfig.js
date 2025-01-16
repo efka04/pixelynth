8f269d35289f42f8b45a693575a849ec2fad552a
efka04 <118260453+efka04@users.noreply.github.com>
2025-01-16 15:39:07 +0900

    Update firebaseConfig.js

--- app/db/firebaseConfig.js
+++ app/db/firebaseConfig.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'; // Ensure Firestore is imported
